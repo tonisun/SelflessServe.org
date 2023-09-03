f931e793bd155742837b232121987aae098804f6
Toni Zeidler <toni.z@gmx.de>
2023-09-03 15:10:21 +0200

    desc for test

--- test/errorMiddleware.test.js
+++ test/errorMiddleware.test.js
@@ -5,36 +5,40 @@ const ErrorHandler = require('../utils/errorHandler');
 
 const { expect } = chai;
 
+// Test Suite for errorMiddleware
 describe('Error Middleware', function() {
-  it('should send a development error response', function() {
-    const err = new ErrorHandler('Test error', 400);
-    const req = { path: '/test' };
-    const res = { status: sinon.stub().returnsThis(), json: sinon.stub() };
-    process.env.NODE_ENV = 'development';
 
-    errorMiddleware(err, req, res, {});
+    // Test Cases: Development Error Response Test
+    it('should send a development error response', function() {
+        const err = new ErrorHandler('Test error', 400);
+        const req = { path: '/test' };
+        const res = { status: sinon.stub().returnsThis(), json: sinon.stub() };
+        process.env.NODE_ENV = 'development';
 
-    expect(res.status.calledWith(400)).to.be.true;
-    expect(res.json.calledWith({
-      success: false,
-      error: err,
-      errMessage: 'Test error',
-      stack: err.stack,
-    })).to.be.true;
-  });
+        errorMiddleware(err, req, res, {});
 
-  it('should send a production error response', function() {
-    const err = new ErrorHandler('Test error', 400);
-    const req = { path: '/test' };
-    const res = { status: sinon.stub().returnsThis(), json: sinon.stub() };
-    process.env.NODE_ENV = 'production';
+        expect(res.status.calledWith(400)).to.be.true;
+        expect(res.json.calledWith({
+        success: false,
+        error: err,
+        errMessage: 'Test error',
+        stack: err.stack,
+        })).to.be.true;
+    });
 
-    errorMiddleware(err, req, res, {});
+    // Production Error Response Test
+    it('should send a production error response', function() {
+        const err = new ErrorHandler('Test error', 400);
+        const req = { path: '/test' };
+        const res = { status: sinon.stub().returnsThis(), json: sinon.stub() };
+        process.env.NODE_ENV = 'production';
 
-    expect(res.status.calledWith(400)).to.be.true;
-    expect(res.json.calledWith({
-      success: false,
-      message: 'Test error',
-    })).to.be.true;
-  });
+        errorMiddleware(err, req, res, {});
+
+        expect(res.status.calledWith(400)).to.be.true;
+        expect(res.json.calledWith({
+        success: false,
+        message: 'Test error',
+        })).to.be.true;
+    });
 });
